1cfe3b182f5376c00ee45ff74a6d990b22586892
Kevin Lin <kevinslin8@gmail.com>
2012-03-11 04:05:17 -0500

    fixed main/urls

--- main/urls.py
+++ main/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('main.views',
     url(r'^$', 'landing_page'),
-    url(r'^index', 'index'),
-    url(r'^example$', 'example'),
-    url(r'^home$', 'home'),
+    url(r'^index/', 'index'),
+    url(r'^example/', 'example'),
+    url(r'^home/', 'home'),
 
     url(r'^pset/(?P<p_id>\d+)/', 'pset'),
 )
